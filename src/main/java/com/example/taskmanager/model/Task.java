3dab4a8ae908c8e32e685e24b97664960875c83e
rhan316 <janek53@protonmail.com>
2024-09-04 11:59:21 +0200

    04.09.2024 12:00

---
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -0,0 +1,63 @@
+package com.example.taskmanager.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+
+    public Task() {}
+
+    public Task(String title, String description, LocalDateTime created_at) {
+        this.title = title;
+        this.description = description;
+        this.created_at = created_at;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+}
